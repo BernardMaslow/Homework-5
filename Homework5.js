fd48e2717b6d6699641ed24a9ba8208c8b00107b
Bernard <mcjaydove@aol.com>
2016-07-07 19:58:45 -0400

    Submitting Homework

---
+++ Homework5.js
@@ -0,0 +1,29 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(a + b);
+
+alert("Hello there!")
+
+var x = 50
+console.log(x * 10);
+
+
+var cars = ["toyota", "honda", "porsche"]
+console.log(cars[1]);
+
+
+var info = {
+  firstName: "bernard",
+  lastName: "maslow",
+  birthday: "06/05/95",
+  interests: ["music", "writing", "producing"]
+}
+console.log(info.birthday);
+console.log(info.interests[2]);
+
+
+var beats = {type: "trap" ,pace: "fast"};
+console.log(beats["type"]);
+
+// comments about javascript 
